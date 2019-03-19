b9a7bdea293faf776553046a3e594757eeb387cd
Ajeet Kumar <Ajeetkmr154@gmail.com>
2019-03-19 23:10:54 +0530

    home controller change

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Vidly.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.....";
 
             return View();
         }
